450a736071e0258998331fb6ad6a7bd2e55ead20
petruha <refloader@gmail.com>
2023-03-05 18:42:14 +0300

    Add docker-compose

---
+++ api/main.py
@@ -0,0 +1,72 @@
+from typing import List
+from fastapi import FastAPI
+
+from models import (
+    LoginPostRequest,
+    LoginPostResponse,
+    ErrorResponse,
+    User,
+    UserRegisterPostRequest,
+    UserRegisterPostResponse,
+)
+
+app = FastAPI(
+    title="OTUS Highload Architect",
+    version="1.0.0",
+)
+
+
+@app.post(
+    "/login",
+    response_model=LoginPostResponse,
+    responses={
+        "500": {"model": ErrorResponse},
+        "503": {"model": ErrorResponse},
+    },
+)
+def post_login(
+    body: LoginPostRequest = None,
+) -> LoginPostResponse | ErrorResponse:
+    pass
+
+
+@app.get(
+    "/user/get/{id}",
+    response_model=User,
+    responses={
+        "500": {"model": ErrorResponse},
+        "503": {"model": ErrorResponse},
+    },
+)
+def get_user_get_id(
+    id: str,
+) -> User | ErrorResponse:
+    pass
+
+
+@app.post(
+    "/user/register",
+    response_model=UserRegisterPostResponse,
+    responses={
+        "500": {"model": ErrorResponse},
+        "503": {"model": ErrorResponse},
+    },
+)
+def post_user_register(
+    body: UserRegisterPostRequest = None,
+) -> UserRegisterPostResponse | ErrorResponse:
+    pass
+
+
+@app.get(
+    "/user/search",
+    response_model=List[User],
+    responses={
+        "500": {"model": ErrorResponse},
+        "503": {"model": ErrorResponse},
+    },
+)
+def get_user_search(
+    first_name: str, last_name: str = ...
+) -> List[User] | ErrorResponse:
+    pass
---
+++ api/models.py
@@ -0,0 +1,49 @@
+
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class User(BaseModel):
+    id: Optional[str] = Field(None, description="Идентификатор пользователя")
+    first_name: Optional[str] = Field(None, description="Имя", example="Имя")
+    second_name: Optional[str] = Field(None, description="Фамилия", example="Фамилия")
+    age: Optional[int] = Field(None, description="Возраст", example=18)
+    biography: Optional[str] = Field(
+        None, description="Интересы", example="Хобби, интересы и т.п."
+    )
+    city: Optional[str] = Field(None, description="Город", example="Москва")
+
+
+class LoginPostRequest(BaseModel):
+    id: Optional[str] = Field(None, example="234")
+    password: Optional[str] = Field(None, example="Секретная строка")
+
+
+class LoginPostResponse(BaseModel):
+    token: Optional[str] = Field(None, example="e4d2e6b0-cde2-42c5-aac3-0b8316f21e58")
+
+
+class ErrorResponse(BaseModel):
+    message: str = Field(..., description="Описание ошибки")
+    request_id: Optional[str] = Field(
+        None,
+        description="Идентификатор запроса. Предназначен для более быстрого поиска проблем.",
+    )
+    code: Optional[int] = Field(
+        None,
+        description="Код ошибки. Предназначен для классификации проблем и более быстрого решения проблем.",
+    )
+
+
+class UserRegisterPostRequest(BaseModel):
+    first_name: Optional[str] = Field(None, example="Имя")
+    second_name: Optional[str] = Field(None, example="Фамилия")
+    age: Optional[int] = Field(None, example=18)
+    biography: Optional[str] = Field(None, example="Хобби, интересы и т.п.")
+    city: Optional[str] = Field(None, example="Москва")
+    password: Optional[str] = Field(None, example="Секретная строка")
+
+
+class UserRegisterPostResponse(BaseModel):
+    user_id: Optional[str] = Field(None, example="e4d2e6b0-cde2-42c5-aac3-0b8316f21e58")
