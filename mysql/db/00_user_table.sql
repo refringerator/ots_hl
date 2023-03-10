c569954b6b3255262862b7a70b56a23c0425466c
petruha <refloader@gmail.com>
2023-03-11 02:51:53 +0300

    Working docker compose

---
+++ mysql/db/00_user_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `user` (
+  `id` BINARY(16) PRIMARY KEY NOT NULL,
+  `first_name` varchar(256) NOT NULL,
+  `second_name` varchar(256) NOT NULL,
+  `password_hash` char(60) NOT NULL,
+  `age` int,
+  `biographt` varchar(256),
+  `city` varchar(256)
+);
